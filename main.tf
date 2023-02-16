3fa410db917e51c9c4df01aa9af6eec395e22be7
Briggendudex <sigurd@sivertsens.net>
2023-02-16 12:21:57 +0100

    appgw-module-next-step-after-that

--- main.tf
+++ main.tf
@@ -38,40 +38,13 @@ resource "azurerm_application_gateway_http_listener" "http_listener" {
 
 # Create an HTTPS listener for the Application Gateway
 
-resource "azurerm_application_gateway_https_listener" "https_listener" {
-  name                           = var.https_listener_name
-  resource_group_name            = var.resource_group_name
-  application_gateway_name       = azurerm_application_gateway.app_gateway.name
-  frontend_ip_configuration_name = "app_gateway_frontend_ip"
-  frontend_port_name             = "app_gateway_frontend_port_https"
-  protocol                       = "Https"
-  ssl_certificate                = base64decode(data.azurerm_key_vault_secret.ssl_cert.value)
-  require_server_name_indication = true
-}
-# How to use
-# Create the Application Gateway
-# module "app_gateway" {
-#   source = "path/to/azure-app-gateway"
-
-#   location                        = var.location
-#   resource_group_name             = var.resource_group_name
-#   subnet_id                       = azurerm_subnet.app_gateway_subnet.id
-#   public_ip_id                    = azurerm_public_ip.app_gateway_public_ip.id
-#   key_vault_name                  = var.key_vault_name
-#   key_vault_sku_name              = var.key_vault_sku_name
-#   ssl_cert_name                   = var.ssl_cert_name
-#   app_gateway_name                = var.app_gateway_name
-#   app_gateway_sku_name            = var.app_gateway_sku_name
-#   app_gateway_capacity            = var.app_gateway_capacity
-#   backend_pool_id                 = azurerm_lb_backend_address_pool.backend_pool.id
-#   http_listener_id                = azurerm_application_gateway_http_listener.http_listener.id
-#   https_listener_id               = azurerm_application_gateway_https_listener.https_listener.id
-#   ssl_cert_value                  = var.ssl_cert_value
-#   virtual_network_name            = var.virtual_network_name
-#   backend_ips                     = var.backend_ips
-# }
-
-# # Output the public IP address of the Application Gateway
-# output "app_gateway_public_ip_address" {
-#   value = azurerm_public_ip.app_gateway_public_ip.ip_address
+# resource "azurerm_application_gateway_https_listener" "https_listener" {
+#   name                           = var.https_listener_name
+#   resource_group_name            = var.resource_group_name
+#   application_gateway_name       = azurerm_application_gateway.app_gateway.name
+#   frontend_ip_configuration_name = "app_gateway_frontend_ip"
+#   frontend_port_name             = "app_gateway_frontend_port_https"
+#   protocol                       = "Https"
+#   ssl_certificate                = base64decode(data.azurerm_key_vault_secret.ssl_cert.value)
+#   require_server_name_indication = true
 # }
